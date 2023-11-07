a4f9a6f31758cb5bac422fae7468705bb4aa7157
Aravind Kodandaramaiah <22780819+aravindkaws@users.noreply.github.com>
2023-11-07 18:00:34 -0500

    Update create-layer.sh
    passed platform option to docker build

--- Kendra-Foundational-LLM-Chatbot/create-layer.sh
+++ Kendra-Foundational-LLM-Chatbot/create-layer.sh
@@ -8,6 +8,6 @@ if [ ! -d "$directory" ]; then
     mkdir -p "$directory"
 fi
 
-docker build --no-cache . -t bedrock-langchain:latest
+docker build --no-cache --platform linux/amd64 . -t bedrock-langchain:latest
 container_id=$(docker run --platform linux/amd64 -t -d bedrock-langchain /bin/bash)
 docker cp $container_id:/tmp/layer/python-bedrock-langchain-layer.zip ./lambda-layer
